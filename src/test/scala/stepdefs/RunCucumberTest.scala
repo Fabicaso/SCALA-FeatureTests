df7dd86b157673a4272e509515ab3a472a82dfaa
agustafson <andrew.gustafson@itv.com>
2016-07-13 10:36:45 +0100

    Add RunCucumberTest to run all cucumber tests

---
+++ src/test/scala/stepdefs/RunCucumberTest.scala
@@ -0,0 +1,14 @@
+package stepdefs
+
+import cucumber.api.CucumberOptions
+import cucumber.api.junit.Cucumber
+import org.junit.runner.RunWith
+
+@RunWith(classOf[Cucumber])
+@CucumberOptions(
+  features = Array("src/test/resources/features"),
+  glue = Array("stepdefs"),
+  plugin = Array("pretty", "html:target/cucumber-report")
+)
+class RunCucumberTest {
+}
