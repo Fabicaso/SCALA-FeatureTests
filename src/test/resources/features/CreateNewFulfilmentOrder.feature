2135376775f4daf85fd540ce0c6b0e004075de16
agustafson <andrew.gustafson@itv.com>
2016-07-13 10:37:11 +0100

    Add first feature test

---
+++ src/test/resources/features/CreateNewFulfilmentOrder.feature
@@ -0,0 +1,11 @@
+Feature: Create a new fulfilment order
+
+  As a GE Ops user
+  I would like to create a new fulfilment order
+  So that I can send it to Deluxe for fulfilment
+
+  Scenario: Navigate to new fulfilment order page from a licence
+    Given I have navigated to the new fulfilment request page
+    And I have entered licence id "LIC-123"
+    When I click to create the new work fulfilment request
+    Then I am taken to the new fulfilment page
---
+++ src/test/scala/stepdefs/CreateNewFulfilmentOrderSteps.scala
@@ -0,0 +1,37 @@
+package stepdefs
+
+import java.util.concurrent.TimeUnit
+
+import cucumber.api.scala.{EN, ScalaDsl}
+import org.openqa.selenium.remote.{DesiredCapabilities, RemoteWebDriver}
+import org.openqa.selenium.{By, WebElement}
+import org.scalatest.Matchers
+
+class CreateNewFulfilmentOrderSteps extends ScalaDsl with EN with Matchers {
+  val capabilities = DesiredCapabilities.chrome()
+  val driver = new RemoteWebDriver(new java.net.URL("http", "10.206.45.185", 8080, "/wd/hub"), capabilities)
+  driver.manage().timeouts().implicitlyWait(30 , TimeUnit.SECONDS)
+
+  Given("""^I have navigated to the new fulfilment request page$""") { () =>
+    driver.navigate().to("http://frontend.stg.fp.itv.com/new-fulfilment-request")
+  }
+
+  And("""^I have entered licence id "(.+)"$""") { (licenceId: String) =>
+    val licenceInput: WebElement = driver.findElement(By.id("licence-input"))
+    println(s"licence input: $licenceInput")
+    licenceInput.sendKeys(licenceId)
+  }
+
+  When("""^I click to create the new work fulfilment request$""") { () =>
+    val selectAssetsElem: WebElement = driver.findElementByClassName("Button") //findElement(By.partialLinkText("Select Assets"))
+
+    println(s"select assets elem: $selectAssetsElem with text '${selectAssetsElem.getText}'")
+    selectAssetsElem.click()
+  }
+
+  Then("""^I am taken to the new fulfilment page$""") { () =>
+    driver.findElementsByTagName("h1") shouldNot be(empty)
+  }
+
+  After(_ => driver.quit())
+}
