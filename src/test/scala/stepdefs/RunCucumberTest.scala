50d1d9ca6572243e14e4ced0f8a2e585376ff107
fabicaso <fabio.caso@itv.com>
2016-07-18 09:25:46 +0100

    New Skip Test

--- src/test/scala/stepdefs/RunCucumberTest.scala
+++ src/test/scala/stepdefs/RunCucumberTest.scala
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith
 
 @RunWith(classOf[Cucumber])
 @CucumberOptions(
-  features = Array("src/test/resources/features"),
+  features = Array("/Users/fabicaso/dev/fulfilmentplanning-featuretests/src/test/resources/features"),
   glue = Array("stepdefs"),
   plugin = Array("pretty", "html:target/cucumber-report", "junit:target/junit-report.xml")
 )
