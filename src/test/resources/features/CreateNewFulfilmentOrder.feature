865f37f7ac97d84480c6264af3d440caa5a05178
fabicaso <fabio.caso@itv.com>
2016-07-25 09:50:35 +0100

    New  E2E Fulfillment Order Creation Test

--- src/test/resources/features/CreateNewFulfilmentOrder.feature
+++ src/test/resources/features/CreateNewFulfilmentOrder.feature
@@ -8,4 +8,4 @@ Feature: Create a new fulfilment order
     Given I have navigated to the new fulfilment request page
     And I have entered licence id "Create Ful Order-123"
     When I click to create the new work fulfilment request
-    Then I am taken to the new fulfilment page
+    Then I have Created a Fulfilment Order: COMPLETE
--- src/test/scala/stepdefs/CreateNewFulfilmentOrderSteps.scala
+++ src/test/scala/stepdefs/CreateNewFulfilmentOrderSteps.scala
@@ -21,17 +21,44 @@ class CreateNewFulfilmentOrderSteps extends ScalaDsl with EN with Matchers {
     val licenceInput: WebElement = driver.findElement(By.id("licence-input"))
     println(s"licence input: $licenceId")
     licenceInput.sendKeys(licenceId)
+
+    val SelectAssetButton: WebElement = driver.findElementByClassName("Button")
+    SelectAssetButton.click()
+    println("Select Asset Button clicked")
+
+    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS)
   }
 
   When("""^I click to create the new work fulfilment request$""") { () =>
-    val selectAssetsElem: WebElement = driver.findElementByXPath("//*[@id='app']/div/main/article/div/div/div[2]/div/form/div[2]/div/button")
 
-    println(s"select assets elem: $selectAssetsElem with text '${selectAssetsElem.getText}'")
-    selectAssetsElem.click()
+    val selectAssetsButton1: WebElement = driver.findElementByXPath("html/body/div[1]/div/main/article/div/div/div[2]/div/div/section[1]/div/ul/li/div[2]/div/button")
+    println("select assets elem button clicked and Format pop-up window open'")
+    selectAssetsButton1.click()
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS)
+
+    val selectSkipButton: WebElement = driver.findElementByXPath("//*[@id='app']/div/main/article/div/div/div[2]/div/div/section[3]/header/div/button")
+    selectSkipButton.click()
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS)
+
+    val selectFormatButton: WebElement = driver.findElementByXPath( "/html/body/div[2]/div/div/div/div[1]/div/table/tbody/tr[4]/td[1]")
+    selectFormatButton.click()
+    println("select Format elem clicked and Format pop-up window closed'")
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS)
+
+    val selectCreateButton: WebElement = driver.findElementByXPath("//*[@id='app']/div/main/article/div/div/div[3]/aside/div[2]/button[1]")
+    selectCreateButton.click()
+
   }
 
-  Then("""^I am taken to the new fulfilment page$""") { () =>
+  Then("""^I have Created a Fulfilment Order: COMPLETE$""") { () =>
     driver.findElementsByTagName("h1") shouldNot be(empty)
+val Complete: WebElement =driver.findElementByXPath("//*[@id='app']/div/main/article/div/div/div[2]/div/h1")
+
+val ComplString : String = Complete.getText()
+    println(s"Fulfillment Order Creation Status:$ComplString")
+
+    assert(ComplString == "Complete")
+
 
     driver.close()
 
